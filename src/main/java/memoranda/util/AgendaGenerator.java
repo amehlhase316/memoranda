44aa3b002daaedc014878191999318e6f7bd4b68
Ben Meddeb <bmeddeb@asu.edu>
2023-05-27 15:16:03 -0400

    Edit Add StickerS

--- src/main/java/memoranda/util/AgendaGenerator.java
+++ src/main/java/memoranda/util/AgendaGenerator.java
@@ -371,7 +371,7 @@ public class AgendaGenerator {
 				 +   "<table border=\"0\" cellpadding=\"0\" width=\"100%\"><tr><td><a href=\"memoranda:addsticker\"><img align=\"left\" width=\"22\" height=\"22\" src=\""				
 				 + iurl
 				+ "\" border=\"0\"  hspace=\"0\" vspace=\"0\" alt=\"New sticker\"></a></td><td width=\"100%\"><a href=\"memoranda:addsticker\"><b>&nbsp;"
-				+Local.getString("Add sticker")+"</b></a></td></tr></table>";
+				+Local.getString("Add stickerS")+"</b></a></td></tr></table>";
 		PriorityQueue pQ = sortStickers();
 		while(!pQ.Vacia()){
 		Element el = pQ.extraer();
