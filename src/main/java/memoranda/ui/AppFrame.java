31f909180825d1ea8b43df1bb74c8b6d6b3f9b7c
rpdinaroASU <133998885+rpdinaroASU@users.noreply.github.com>
2023-05-30 12:38:04 -0600

    Fix broken branch on Window Event Proccessing to restore minimization

--- src/main/java/memoranda/ui/AppFrame.java
+++ src/main/java/memoranda/ui/AppFrame.java
@@ -689,8 +689,7 @@ public class AppFrame extends JFrame {
         }
         else if ((e.getID() == WindowEvent.WINDOW_ICONIFIED)) {
             super.processWindowEvent(new WindowEvent(this,
-                    WindowEvent.WINDOW_CLOSING));
-            doMinimize();
+                    WindowEvent.WINDOW_ICONIFIED));
         }
         else
             super.processWindowEvent(e);
