8e7b78349b494bef183a22d89fe66e9f0b8c1fc7
Paul Carmichael <pccarmic@asu.edu>
2023-05-31 13:52:48 -0700

    User Story #4 Task #23 Updated output language in ExportSticker.java and ImportSticker.java from spanish to english

--- src/main/java/memoranda/ui/ImportSticker.java
+++ src/main/java/memoranda/ui/ImportSticker.java
@@ -19,7 +19,7 @@ String name;
                   
                   */
                 
-                JOptionPane.showMessageDialog(null,Local.getString("Aun no podemos importar su documento"));
+                JOptionPane.showMessageDialog(null,Local.getString("We still can't import your document"));
                 return true;
         }
         
