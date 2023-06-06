da9ac20dbf2f6642704cabc6a524ab07f7b79caf
jblichar <jblichar@asu.edu>
2023-06-05 22:14:27 -0600

    Added minimizeWindow method

--- src/main/java/memoranda/ui/App.java
+++ src/main/java/memoranda/ui/App.java
@@ -144,6 +144,12 @@ public class App {
 		frame.dispose();
 	}
 
+	public static void minimizeWindow() {
+		if (frame == null)
+			return;
+		frame.setState(AppFrame.ICONIFIED);
+	}
+
 	/**
 	 * Method showSplash.
 	 */
