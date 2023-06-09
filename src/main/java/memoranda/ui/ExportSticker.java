e08ab092d978cc85c8c73822b1640e9349059007
Paul Carmichael <pccarmic@asu.edu>
2023-06-08 22:50:03 -0500

    US#4 Task #23 Updated language in AgendaGenerator.java, ExportSticker.java, and AgendaPanel.java from spanish to english.

--- src/main/java/memoranda/ui/ExportSticker.java
+++ src/main/java/memoranda/ui/ExportSticker.java
@@ -69,12 +69,12 @@ public class ExportSticker {
                         fwrite.write(contents);
                         
                         fwrite.close();
-                        JOptionPane.showMessageDialog(null,Local.getString("Documento creado con exito en su carpeta Memoranda =D"));
+                        JOptionPane.showMessageDialog(null,Local.getString("Document created successfully in your Memoranda folder =D"));
             
             
         } catch (IOException e) {
             e.printStackTrace();
-            JOptionPane.showMessageDialog(null,Local.getString("NO Logramos crear su documento =(..."));
+            JOptionPane.showMessageDialog(null,Local.getString("We were NOT able to create your document =(..."));
         }
                 
                 
