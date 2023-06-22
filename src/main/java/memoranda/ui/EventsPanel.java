cbc900b90465ffd69f65e9843876b80ffdbbe4db
Paul Carmichael <pccarmic@asu.edu>
2023-06-22 16:35:45 -0500

    US#28 Task#30 Changed the background color of the Event Panel

--- src/main/java/memoranda/ui/EventsPanel.java
+++ src/main/java/memoranda/ui/EventsPanel.java
@@ -132,7 +132,7 @@ public class EventsPanel extends JPanel {
             new ImageIcon(main.java.memoranda.ui.AppFrame.class.getResource("/ui/icons/event_remove.png")));
 
         this.setLayout(borderLayout1);
-        scrollPane.getViewport().setBackground(Color.white);
+        scrollPane.getViewport().setBackground(Color.orange);
         eventsTable.setMaximumSize(new Dimension(32767, 32767));
         eventsTable.setRowHeight(24);
         eventPPMenu.setFont(new java.awt.Font("Dialog", 1, 10));
