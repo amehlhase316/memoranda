8ed6134c1de2450e96acffec3ce48a20a69e0e86
amoralesj <76403950+amoralesj@users.noreply.github.com>
2023-06-24 16:54:13 -0400

    Have modified class to handle reading and writing of Routes json objects

--- src/main/java/memoranda/JsonHandler.java
+++ src/main/java/memoranda/JsonHandler.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 
@@ -17,26 +18,31 @@ public class JsonHandler {
 
 	public List<Node> nodes;
     public List<String> nodesString;
+
+    public List<String> driversToString;
+    public List<String> busesToString;
 	public ArrayList<Driver> driverList;
     public ArrayList<Bus> busList;
-	
-	public JsonHandler() {
-		nodes = new ArrayList<Node>();
+    private ArrayList<Route> routeList;
+
+    public JsonHandler() {
+        nodes = new ArrayList<Node>();
         nodesString = new ArrayList<String>();
         driverList = new ArrayList<Driver>();
+        driversToString = new ArrayList<String>();
         busList = new ArrayList<Bus>();
-	}
-    public void addNodeString(String option) {
-        nodesString.add(option);
-    }
-	public void addNode(String id, double latitude, double longitude) {
-        nodes.add(new Node(id, latitude, longitude));
-    }
-	
-	public void addDriver(String id, String name, String phoneNumber) {
-		driverList.add(new Driver(id, name, phoneNumber));
-	}
+        busesToString = new ArrayList<String>();
+        routeList = new ArrayList<Route>();
 
+    }
+
+    private JSONObject createJsonObject(Node node) {
+        JSONObject object = new JSONObject();
+        object.put("id", node.getId());
+        object.put("lat", node.getLatitude());
+        object.put("lon", node.getLongitude());
+        return object;
+    }
 	public void readNodesFromJSON(String filename) {
         try {
         	JSONParser parser = new JSONParser();
@@ -83,43 +89,85 @@ public class JsonHandler {
 
     public void writeRouteToJson(Route route) {
         try {
-                JSONObject routeObject = new JSONObject();
-                JSONArray nodesArray = new JSONArray();
+            // Read the existing JSON file and parse it into a JSONObject
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(new FileReader("nodes1.json"));
 
-            for (Object obj : route.getNodes()) {
-                JSONObject o = new JSONObject();
-                String id = (String) o.get("id");
-                Node nodeX = nodes.stream().filter(node -> node.getId().equals(id)).findFirst().orElse(null);
-                if (nodeX != null) {
-                    JSONObject node1Object = createJsonObject(nodeX);
-                    nodesArray.add(node1Object);
+            // Create a new JSONArray to store all routes (existing + new)
+            JSONArray routesArray = new JSONArray();
+
+            // Create a new JSONObject for the current route
+            JSONObject currentRouteObj = new JSONObject();
+
+            // Create a new JSONArray to store all nodes of the current route
+            JSONArray currentRouteNodesArray = new JSONArray();
+            JSONObject nodeObj = new JSONObject();
+            nodeObj.put("Driver", route.getDriver().getName());
+            nodeObj.put("Bus ID", route.getBus().getId());
+            nodeObj.put("Stop Duration", route.getStopDuration());
+            for (Node node : route.getNodes()) {
+
+                nodeObj.put("Id", node.getId());
+                nodeObj.put("Longitude", node.getLongitude());
+                nodeObj.put("Latitude", node.getLatitude());
+
+                currentRouteNodesArray.add(nodeObj);
+            }
+
+            // Add the nodes array to the current route object
+            currentRouteObj.put("Route", currentRouteNodesArray);
+
+            // Add the current route object to the routes array
+            routesArray.add(currentRouteObj);
+
+            // Get the existing routes from the JSON object (if any) and add them to routesArray
+            JSONArray existingRoutesArray = (JSONArray) jsonObject.get("Routes");
+            if (existingRoutesArray != null) {
+                routesArray.addAll(existingRoutesArray);
+            }
+
+            // Update the routes array in the JSONObject
+            jsonObject.put("Routes", routesArray);
+
+            // Write the updated JSONObject to the file
+            FileWriter fileWriter = new FileWriter("nodes1.json");
+            fileWriter.write(jsonObject.toJSONString());
+            fileWriter.flush();
+            fileWriter.close();
+            System.out.println("Routes have been written to and saved in nodes1.json");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readRoutesFromJSON() {
+        try {
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(new FileReader("nodes1.json"));
+
+            JSONArray routesArray = (JSONArray) jsonObject.get("Routes");
+            List<Node> listOfNodes= new ArrayList<Node>();
+            Double stopDuration = 0.0;
+            for (Object obj : routesArray) {
+                JSONObject routeObj = (JSONObject) obj;
+                if(routeObj.get("Stop Duration").equals("Stop Duration")) {
+                    stopDuration = (Double) routeObj.get("Stop Duration");
                 }
-                routeObject.put("Route", nodesArray);
+                String id = (String) routeObj.get("Id");
+                Double longitude = (Double) routeObj.get("Longitude");
+                Double latitude = (Double) routeObj.get("Latitude");
+                listOfNodes.add(new Node(id,latitude,longitude));
             }
 
-                // Write the JSON object to the file
-                FileWriter fileWriter = new FileWriter("nodes1.json");
-                fileWriter.write(routeObject.toJSONString());
-                fileWriter.flush();
-                fileWriter.close();
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            addRoute(listOfNodes, stopDuration);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
 
-        private JSONObject createJsonObject(Node node) {
-            JSONObject object = new JSONObject();
-            object.put("id", node.getId());
-            object.put("lat", node.getLatitude());
-            object.put("lon", node.getLongitude());
-            return object;
-        }
-
-
-
-    public List<Node> getNodes() {
-        return nodes;
+    private void addRoute(List<Node> listOfNodes, Double sd) {
+        this.routeList.add(new Route((ArrayList<Node>) listOfNodes, sd));
     }
 
     public List<String> getNodesString() {
@@ -137,7 +185,7 @@ public class JsonHandler {
                 String id = (String) nodeObj.get("id");
                 String name = (String) nodeObj.get("name");
                 String phoneNumber = (String) nodeObj.get("phoneNumber");
-
+                addDriverToString(name);
                 addDriver(id, name, phoneNumber);
             }
         } catch (Exception e) {
@@ -158,6 +206,7 @@ public class JsonHandler {
                 int seats = Integer.valueOf((String) nodeObj.get("seats"));
                 Bus tempBus = new Bus(id, seats);
                 tempBus.setAssignedDriver((String) nodeObj.get("assignedDriver"));
+                addBusToString(id);
                 busList.add(tempBus);
             }
         } catch (Exception e) {
@@ -230,4 +279,49 @@ public class JsonHandler {
             e.printStackTrace();
         }
     }
+
+    public List<String> getDriversToString() {
+        return driversToString;
+    }
+
+    public List<String> getBusesToString() {
+        return busesToString;
+    }
+    public List<Node> getNodes() {
+        return nodes;
+    }
+    public ArrayList<Route> getRouteList() {
+        return routeList;
+    }
+    public void addNodeString(String option) {
+        nodesString.add(option);
+    }
+
+    private void addDriverToString(String name) {
+        driversToString.add(name);
+    }
+
+    private void addBusToString(String id) {
+        busesToString.add(id);
+    }
+    public void addNode(String id, double latitude, double longitude) {
+        nodes.add(new Node(id, latitude, longitude));
+    }
+
+    public void addDriver(String id, String name, String phoneNumber) {
+        driverList.add(new Driver(id, name, phoneNumber));
+    }
+
+
+
+    public ArrayList<Driver> getDriverList() {
+        return driverList;
+    }
+
+
+
+    public ArrayList<Bus> getBusList() {
+        return busList;
+    }
+
 }
