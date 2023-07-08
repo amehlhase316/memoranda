b9d42cbe9409d5250cbba7ea7fc2a3346e48a68a
jblichar <jblichar@asu.edu>
2023-07-07 20:26:10 -0600

    Added user lesson functionality and revised UI

--- src/main/java/memoranda/ui/HomePanel.java
+++ src/main/java/memoranda/ui/HomePanel.java
@@ -39,7 +39,6 @@ public class HomePanel extends JPanel {
 	JButton loginB = new JButton();
 	JToolBar toolBar = new JToolBar();
 	JButton logoutB = new JButton();
-	JButton export = new JButton();
 	JEditorPane viewer = new JEditorPane("text/html", "");
 	JEditorPane viewer1 = new JEditorPane("text/html", "");
 	JEditorPane viewer2 = new JEditorPane("text/html", "");
@@ -82,7 +81,13 @@ public class HomePanel extends JPanel {
 
 	GridBagConstraints gbc = new GridBagConstraints();
 
+	String[][] data = {};
+	String[] column ={"Class ID","Class Time","Class Date"};
+	JTable jt = new JTable(data,column);
 
+	JLabel lessons = new JLabel("", 0);
+
+	JLabel loggedOut = new JLabel("<html>Please login to view<br/>Username: login<br/>Password: password</html>", SwingConstants.CENTER);
 
 
 	//	JPopupMenu agendaPPMenu = new JPopupMenu();
@@ -298,6 +303,269 @@ public class HomePanel extends JPanel {
 
 		this.setLayout(borderLayout1);
 		scrollPane.getViewport().setBackground(Color.red);
+		panel.setBackground(Color.WHITE);
+		panel.setSize(getWidth(), getHeight());
+
+		panel.setLayout(layout);
+		panel.add(loggedOut);
+		this.add(panel);
+
+		scrollPane.getViewport().add(viewer, null);
+		notePanel.getViewport().add(viewer1, null);
+		toolBar.add(loginB, null);
+		toolBar.add(logoutB, null);
+		toolBar.addSeparator(new Dimension(8, 24));
+
+		this.add(toolBar, BorderLayout.NORTH);
+
+
+		CurrentDate.addDateListener(new DateListener() {
+			public void dateChange(CalendarDate d) {
+				if (isActive)
+					refresh(d);
+			}
+		});
+		CurrentProject.addProjectListener(new ProjectListener() {
+
+			public void projectChange(
+					Project prj,
+					NoteList nl,
+					TaskList tl,
+					ResourcesList rl) {
+			}
+
+			public void projectWasChanged() {
+				if (isActive)
+					refresh(CurrentDate.get());
+			}});
+		EventsScheduler.addListener(new EventNotificationListener() {
+			public void eventIsOccured(main.java.memoranda.Event ev) {
+				if (isActive)
+					refresh(CurrentDate.get());
+			}
+
+			public void eventsChanged() {
+				if (isActive)
+					refresh(CurrentDate.get());
+			}
+		});
+		refresh(CurrentDate.get());
+
+		//        agendaPPMenu.setFont(new java.awt.Font("Dialog", 1, 10));
+		//        agendaPPMenu.add(ppShowActiveOnlyChB);
+		//        PopupListener ppListener = new PopupListener();
+		//        viewer.addMouseListener(ppListener);
+		//		ppShowActiveOnlyChB.setFont(new java.awt.Font("Dialog", 1, 11));
+		//		ppShowActiveOnlyChB.setText(
+		//			Local.getString("Show Active only"));
+		//		ppShowActiveOnlyChB.addActionListener(new java.awt.event.ActionListener() {
+		//			public void actionPerformed(ActionEvent e) {
+		//				toggleShowActiveOnly_actionPerformed(e);
+		//			}
+		//		});		
+		//		boolean isShao =
+		//			(Context.get("SHOW_ACTIVE_TASKS_ONLY") != null)
+		//				&& (Context.get("SHOW_ACTIVE_TASKS_ONLY").equals("true"));
+		//		ppShowActiveOnlyChB.setSelected(isShao);
+		//		toggleShowActiveOnly_actionPerformed(null);		
+	}
+
+	public void refresh(CalendarDate date) {
+		//viewer.setText(HomeGenerator.getUserInfo(date,expandedTasks));
+		viewer1.setText(HomeGenerator.getUserNotes(date));
+		//viewer2.setText((HomeGenerator.getUserClasses(date)));
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				if(gotoTask != null) {
+					viewer.scrollToReference(gotoTask);
+					scrollPane.setViewportView(viewer);
+					//Show notes in note panel
+					//Uses html, possibly change in future
+					notePanel.setViewportView(viewer1);
+					//Show events in enrolled class panel
+					//Uses html, possibly change in future
+					//upcomingClassPanel.setViewportView(viewer2);
+					Util.debug("Set view port to " + gotoTask);
+				}
+			}
+		});
+
+		Util.debug("Summary updated.");
+	}
+
+	public void setActive(boolean isa) {
+		isActive = isa;
+	}
+
+	//	void toggleShowActiveOnly_actionPerformed(ActionEvent e) {
+	//		Context.put(
+	//			"SHOW_ACTIVE_TASKS_ONLY",
+	//			new Boolean(ppShowActiveOnlyChB.isSelected()));
+	//		/*if (taskTable.isShowActiveOnly()) {
+	//			// is true, toggle to false
+	//			taskTable.setShowActiveOnly(false);
+	//			//showActiveOnly.setToolTipText(Local.getString("Show Active Only"));			
+	//		}
+	//		else {
+	//			// is false, toggle to true
+	//			taskTable.setShowActiveOnly(true);
+	//			showActiveOnly.setToolTipText(Local.getString("Show All"));			
+	//		}*/	    
+	//		refresh(CurrentDate.get());
+	////		parentPanel.updateIndicators();
+	//		//taskTable.updateUI();
+	//	}
+
+	//    class PopupListener extends MouseAdapter {
+	//
+	//        public void mouseClicked(MouseEvent e) {
+	//        	System.out.println("mouse clicked!");
+	////			if ((e.getClickCount() == 2) && (taskTable.getSelectedRow() > -1))
+	////				editTaskB_actionPerformed(null);
+	//		}
+	//
+	//		public void mousePressed(MouseEvent e) {
+	//        	System.out.println("mouse pressed!");
+	//			maybeShowPopup(e);
+	//		}
+	//
+	//		public void mouseReleased(MouseEvent e) {
+	//        	System.out.println("mouse released!");
+	//			maybeShowPopup(e);
+	//		}
+	//
+	//		private void maybeShowPopup(MouseEvent e) {
+	//			if (e.isPopupTrigger()) {
+	//				agendaPPMenu.show(e.getComponent(), e.getX(), e.getY());
+	//			}
+	//		}
+	//
+	//    }
+
+	/**
+	 Method: loginFrame
+	 Inputs:
+	 Returns: JFrame
+	 Description: Creates pop up when login button is pressed.
+	 Confirms user login validity. If valid, displays user info.
+	 */
+	public  JFrame loginFrame() {
+		JPanel panelLogin = new JPanel();
+		panelLogin.setLayout(null);
+
+		JFrame frame = new JFrame();
+		frame.setTitle("Login Page");
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.add(panelLogin);
+		frame.setSize(new Dimension(400, 200));
+
+		JLabel label = new JLabel();
+		label.setBounds(100, 8, 70, 20);
+		panelLogin.add(label);
+
+		JTextField username = new JTextField("Username");
+		username.setBounds(100, 27, 200, 30);
+		panelLogin.add(username);
+
+		JTextField password = new JTextField("Password");
+		password.setBounds(100, 55, 200, 30);
+		panelLogin.add(password);
+
+		JButton button = new JButton("Login");
+		button.setBounds(100, 110, 200, 30);
+		button.setForeground(Color.WHITE);
+		button.setBackground(Color.BLACK);
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String userName = username.getText();
+				String userPassword = password.getText();
+
+				if (users.containsKey(userName)) {
+					user = users.get(userName);
+					if (user.getPassword().equals(userPassword)) {
+						user.login();
+						loggedIn = true;
+
+						panel.remove(loggedOut);
+
+						loginHomePanel();
+
+						JOptionPane.showMessageDialog(null, "Login Successful!");
+						frame.setVisible(false);
+					}
+					else {
+						JOptionPane.showMessageDialog(null, "Password Incorrect");
+					}
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "Username does not exist in system");
+				}
+
+			}
+		});
+		panelLogin.add(button);
+
+		return frame;
+	}
+
+	/**
+	 Method: logoutFrame
+	 Inputs:
+	 Returns: JFrame
+	 Description: Creates pop up when logout button is pressed.
+	 Confirms user logout. If confirmed, changes info displayed..
+	 */
+	public JFrame logoutFrame() {
+		JPanel panelLogout = new JPanel();
+		panelLogout.setLayout(null);
+
+		JFrame frame = new JFrame();
+		frame.setTitle("Logout Page");
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.add(panelLogout);
+		frame.setSize(new Dimension(400, 200));
+
+		JLabel label = new JLabel("Are you sure you want to logout?");
+		label.setBounds(100, 60, 200, 20);
+		panelLogout.add(label);
+
+		JButton button = new JButton("Confirm");
+		button.setBounds(100, 110, 200, 30);
+		button.setForeground(Color.WHITE);
+		button.setBackground(Color.BLACK);
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				user.login();
+				user = null;
+				loggedIn = false;
+
+				panel.remove(userPanel);
+				panel.remove(upcomingClassPanel);
+				panel.remove(addNotesPanel);
+
+				launchHomePanel();
+
+				JOptionPane.showMessageDialog(null, "Logout Successful!");
+				frame.setVisible(false);
+			}
+		});
+		panelLogout.add(button);
+
+		return frame;
+	}
+
+	/**
+	 Method: loginHomePanel
+	 Inputs:
+	 Returns:
+	 Description: Recreates home panel after user login.
+	 */
+	public void loginHomePanel() {
+		this.setLayout(borderLayout1);
 		panel.setBackground(Color.darkGray);
 		panel.setSize(getWidth(), getHeight());
 
@@ -307,31 +575,31 @@ public class HomePanel extends JPanel {
 
 		//Create user info panels
 		//Create first name panel
-		userFirstNameLabel.setText("Login to view");
+		userFirstNameLabel.setText(user.getFirstName());
 		userFirstName.add(userFirstNameLabel);
 		Border userFirstNameTitleBorder = BorderFactory.createTitledBorder(bevelBorder, "First Name", TitledBorder.CENTER, TitledBorder.TOP);
 		userFirstName.setBorder(userFirstNameTitleBorder);
 
 		//Create last name panel
-		userLastNameLabel.setText("Login to view");
+		userLastNameLabel.setText(user.getLastName());
 		userLastName.add(userLastNameLabel);
 		Border userLastNameTitleBorder = BorderFactory.createTitledBorder(bevelBorder, "Last Name", TitledBorder.CENTER, TitledBorder.TOP);
 		userLastName.setBorder(userLastNameTitleBorder);
 
 		//Create user id panel
-		userIDLabel.setText("Login to view");
+		userIDLabel.setText(user.getUsername());
 		userID.add(userIDLabel);
 		Border userIDTitleBorder = BorderFactory.createTitledBorder(bevelBorder, "Username", TitledBorder.CENTER, TitledBorder.TOP);
 		userID.setBorder(userIDTitleBorder);
 
 		//Create user rank panel
-		userRankLabel.setText("Login to view");
+		userRankLabel.setText("" + user.getRank());
 		userRank.add(userRankLabel);
 		Border userRankTitleBorder = BorderFactory.createTitledBorder(bevelBorder, "Belt Rank", TitledBorder.CENTER, TitledBorder.TOP);
 		userRank.setBorder(userRankTitleBorder);
 
 		//Create join date panel
-		userJoinDateLabel.setText("Login to view");
+		userJoinDateLabel.setText("" + user.getJoinDate());
 		userJoinDate.add(userJoinDateLabel);
 		Border userJoinDateTitleBorder = BorderFactory.createTitledBorder(bevelBorder, "Date Joined", TitledBorder.CENTER, TitledBorder.TOP);
 		userJoinDate.setBorder(userJoinDateTitleBorder);
@@ -354,10 +622,20 @@ public class HomePanel extends JPanel {
 		panel.add(userPanel, gbc);
 
 
+		if (user.getLessons() == null) {
+			lessons.setText("No upcoming classes");
+			upcomingClassPanel = new JScrollPane(lessons);
+		} else {
+			while (user.getLessons() != null) {
+				//lessonList = user.getLessons();
+			}
+			upcomingClassPanel = new JScrollPane(jt);
+		}
 		//Create panel to show enrolled classes
 		Border classTitleBorder = BorderFactory.createTitledBorder(bevelBorder, "Enrolled Classes", TitledBorder.LEFT, TitledBorder.TOP);
 		upcomingClassPanel.setBackground(Color.lightGray);
 		upcomingClassPanel.setBorder(classTitleBorder);
+
 		gbc.fill = GridBagConstraints.BOTH;
 		gbc.weightx = 0.67;
 		gbc.weighty = .5;
@@ -451,259 +729,7 @@ public class HomePanel extends JPanel {
 		gbc.gridx = 1;
 		gbc.gridy = 0;
 		panel.add(addNotesPanel, gbc);
-		this.add(panel);
 
-		scrollPane.getViewport().add(viewer, null);
-		notePanel.getViewport().add(viewer1, null);
-		upcomingClassPanel.getViewport().add(viewer2, null);
-		//this.add(userPanel, BorderLayout.CENTER);
-		//this.add(upcomingClassPanel, BorderLayout.EAST);
-		//this.add(addNotesPanel, BorderLayout.SOUTH);
-		toolBar.add(loginB, null);
-		toolBar.add(logoutB, null);
-		toolBar.addSeparator(new Dimension(8, 24));
-
-		this.add(toolBar, BorderLayout.NORTH);
-
-
-		CurrentDate.addDateListener(new DateListener() {
-			public void dateChange(CalendarDate d) {
-				if (isActive)
-					refresh(d);
-			}
-		});
-		CurrentProject.addProjectListener(new ProjectListener() {
-
-			public void projectChange(
-					Project prj,
-					NoteList nl,
-					TaskList tl,
-					ResourcesList rl) {
-			}
-
-			public void projectWasChanged() {
-				if (isActive)
-					refresh(CurrentDate.get());
-			}});
-		EventsScheduler.addListener(new EventNotificationListener() {
-			public void eventIsOccured(main.java.memoranda.Event ev) {
-				if (isActive)
-					refresh(CurrentDate.get());
-			}
-
-			public void eventsChanged() {
-				if (isActive)
-					refresh(CurrentDate.get());
-			}
-		});
-		refresh(CurrentDate.get());
-
-		//        agendaPPMenu.setFont(new java.awt.Font("Dialog", 1, 10));
-		//        agendaPPMenu.add(ppShowActiveOnlyChB);
-		//        PopupListener ppListener = new PopupListener();
-		//        viewer.addMouseListener(ppListener);
-		//		ppShowActiveOnlyChB.setFont(new java.awt.Font("Dialog", 1, 11));
-		//		ppShowActiveOnlyChB.setText(
-		//			Local.getString("Show Active only"));
-		//		ppShowActiveOnlyChB.addActionListener(new java.awt.event.ActionListener() {
-		//			public void actionPerformed(ActionEvent e) {
-		//				toggleShowActiveOnly_actionPerformed(e);
-		//			}
-		//		});		
-		//		boolean isShao =
-		//			(Context.get("SHOW_ACTIVE_TASKS_ONLY") != null)
-		//				&& (Context.get("SHOW_ACTIVE_TASKS_ONLY").equals("true"));
-		//		ppShowActiveOnlyChB.setSelected(isShao);
-		//		toggleShowActiveOnly_actionPerformed(null);		
-	}
-
-	public void refresh(CalendarDate date) {
-		//viewer.setText(HomeGenerator.getUserInfo(date,expandedTasks));
-		viewer1.setText(HomeGenerator.getUserNotes(date));
-		viewer2.setText((HomeGenerator.getUserClasses(date)));
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				if(gotoTask != null) {
-					viewer.scrollToReference(gotoTask);
-					scrollPane.setViewportView(viewer);
-					//Show notes in note panel
-					//Uses html, possibly change in future
-					notePanel.setViewportView(viewer1);
-					//Show events in enrolled class panel
-					//Uses html, possibly change in future
-					upcomingClassPanel.setViewportView(viewer2);
-					Util.debug("Set view port to " + gotoTask);
-				}
-			}
-		});
-
-		Util.debug("Summary updated.");
-	}
-
-	public void setActive(boolean isa) {
-		isActive = isa;
-	}
-
-	//	void toggleShowActiveOnly_actionPerformed(ActionEvent e) {
-	//		Context.put(
-	//			"SHOW_ACTIVE_TASKS_ONLY",
-	//			new Boolean(ppShowActiveOnlyChB.isSelected()));
-	//		/*if (taskTable.isShowActiveOnly()) {
-	//			// is true, toggle to false
-	//			taskTable.setShowActiveOnly(false);
-	//			//showActiveOnly.setToolTipText(Local.getString("Show Active Only"));			
-	//		}
-	//		else {
-	//			// is false, toggle to true
-	//			taskTable.setShowActiveOnly(true);
-	//			showActiveOnly.setToolTipText(Local.getString("Show All"));			
-	//		}*/	    
-	//		refresh(CurrentDate.get());
-	////		parentPanel.updateIndicators();
-	//		//taskTable.updateUI();
-	//	}
-
-	//    class PopupListener extends MouseAdapter {
-	//
-	//        public void mouseClicked(MouseEvent e) {
-	//        	System.out.println("mouse clicked!");
-	////			if ((e.getClickCount() == 2) && (taskTable.getSelectedRow() > -1))
-	////				editTaskB_actionPerformed(null);
-	//		}
-	//
-	//		public void mousePressed(MouseEvent e) {
-	//        	System.out.println("mouse pressed!");
-	//			maybeShowPopup(e);
-	//		}
-	//
-	//		public void mouseReleased(MouseEvent e) {
-	//        	System.out.println("mouse released!");
-	//			maybeShowPopup(e);
-	//		}
-	//
-	//		private void maybeShowPopup(MouseEvent e) {
-	//			if (e.isPopupTrigger()) {
-	//				agendaPPMenu.show(e.getComponent(), e.getX(), e.getY());
-	//			}
-	//		}
-	//
-	//    }
-
-	/**
-	 Method: loginFrame
-	 Inputs:
-	 Returns: JFrame
-	 Description: Creates pop up when login button is pressed.
-	 Confirms user login validity. If valid, displays user info.
-	 */
-	public  JFrame loginFrame() {
-		JPanel panel = new JPanel();
-		panel.setLayout(null);
-
-		JFrame frame = new JFrame();
-		frame.setTitle("Login Page");
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		frame.add(panel);
-		frame.setSize(new Dimension(400, 200));
-
-		JLabel label = new JLabel();
-		label.setBounds(100, 8, 70, 20);
-		panel.add(label);
-
-		JTextField username = new JTextField("Username");
-		username.setBounds(100, 27, 200, 30);
-		panel.add(username);
-
-		JTextField password = new JTextField("Password");
-		password.setBounds(100, 55, 200, 30);
-		panel.add(password);
-
-		JButton button = new JButton("Login");
-		button.setBounds(100, 110, 200, 30);
-		button.setForeground(Color.WHITE);
-		button.setBackground(Color.BLACK);
-		button.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String userName = username.getText();
-				String userPassword = password.getText();
-
-				if (users.containsKey(userName)) {
-					user = users.get(userName);
-					if (user.getPassword().equals(userPassword)) {
-						user.login();
-						loggedIn = true;
-						userFirstNameLabel.setText(user.getFirstName());
-						userLastNameLabel.setText(user.getLastName());
-						userIDLabel.setText(user.getUsername());
-						String rank = "" + user.getRank();
-						userRankLabel.setText(rank);
-						String date = "" + user.getJoinDate();
-						userJoinDateLabel.setText(date);
-						JOptionPane.showMessageDialog(null, "Login Successful!");
-						frame.setVisible(false);
-					}
-					else {
-						JOptionPane.showMessageDialog(null, "Password Incorrect");
-					}
-				}
-				else {
-					JOptionPane.showMessageDialog(null, "Username does not exist in system");
-				}
-
-			}
-		});
-		panel.add(button);
-
-		return frame;
-	}
-
-	/**
-	 Method: logoutFrame
-	 Inputs:
-	 Returns: JFrame
-	 Description: Creates pop up when logout button is pressed.
-	 Confirms user logout. If confirmed, changes info displayed..
-	 */
-	public JFrame logoutFrame() {
-		JPanel panel = new JPanel();
-		panel.setLayout(null);
-
-		JFrame frame = new JFrame();
-		frame.setTitle("Logout Page");
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		frame.add(panel);
-		frame.setSize(new Dimension(400, 200));
-
-		JLabel label = new JLabel("Are you sure you want to logout?");
-		label.setBounds(100, 0, 200, 20);
-		panel.add(label);
-
-		JButton button = new JButton("Confirm");
-		button.setBounds(100, 110, 200, 30);
-		button.setForeground(Color.WHITE);
-		button.setBackground(Color.BLACK);
-		button.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				user.login();
-				user = null;
-				loggedIn = false;
-
-				userFirstNameLabel.setText("Login to view");
-				userLastNameLabel.setText("Login to view");
-				userIDLabel.setText("Login to view");
-				userRankLabel.setText("Login to view");
-				userJoinDateLabel.setText("Login to view");
-
-				JOptionPane.showMessageDialog(null, "Logout Successful!");
-				frame.setVisible(false);
-			}
-		});
-		panel.add(button);
-
-		return frame;
+		panel.revalidate();
 	}
 }
