98dcaec33c29440a0bc5f47341b5a4b469b8c5b9
sjgarc20 <127725103+sjgarc20@users.noreply.github.com>
2023-07-09 02:04:26 -0600

    Remove old test code

--- src/main/java/memoranda/UserList.java
+++ src/main/java/memoranda/UserList.java
@@ -10,8 +10,6 @@ import java.util.HashMap;
  */
 public class UserList {
     public static HashMap<String, User> users = new HashMap<>();
-//    User user;
-//    User user1;
 
     private static UserList INSTANCE;
 
@@ -23,20 +21,6 @@ public class UserList {
     }
 
     public UserList() {
-//        user = new User();
-//        user.setLastName("Smith");
-//        user.setFirstName("John");
-//        user.setUsername("login");
-//        user.setPassword("password");
-//
-//        user1 = new User();
-//        user1.setLastName("Jones");
-//        user1.setFirstName("Arnold");
-//        user1.setUsername("1234");
-//        user1.setPassword("password");
-//
-//        users.put(user.getUsername(), user);
-//        users.put(user1.getUsername(), user1);
     }
 
     /**
